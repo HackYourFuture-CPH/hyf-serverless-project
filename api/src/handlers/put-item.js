441b1ef57f35d1a89a1ca2d748c2493afac33188
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2021-10-17 17:38:02 +0200

    fix put-item.js

--- api/src/handlers/put-item.js
+++ api/src/handlers/put-item.js
@@ -1,6 +1,9 @@
 const dynamodb = require("aws-sdk/clients/dynamodb");
+const docClient = new dynamodb.DocumentClient();
+const aws = require("aws-sdk"); 
 
-exports.putItemHandler = async (event, context) => {
+const tableName = process.env.personsTable;
+exports.putPersonsHandler = async (event, context) => {
   if (event.httpMethod !== "POST") {
     throw new Error(
       `postMethod only accepts POST method, you tried: ${event.httpMethod} method.`
@@ -9,22 +12,33 @@ exports.putItemHandler = async (event, context) => {
   console.info("received:", event);
 
   const body = JSON.parse(event.body);
-  const id = body.id;
-  const name = body.name;
-  const companyName = body.companyName;
+  var params = {
+    TableName: tableName,
+    Item: {
+      id: body.id,
+      fullname: body.fullname,
+      classNr: body.classNr,
+      position: body.position,
+      company: body.company,
+      aboutJob: body.aboutJob,
+      interviewRounds: body.interviewRounds,
+      assignment: body.assignment,
+      imageUrl: "?",
+    },
+  };
+
+  const result = await docClient.put(params).promise();
 
   const response = {
     statusCode: 200,
     body: JSON.stringify(body),
   };
 
-  const aws = require("aws-sdk"); // at the beginning of the files
-
   // after results in put
   const sns = new aws.SNS();
   const paramsSNS = {
-    Message: `id: ${id} Name: ${name} got placed in ${companyName}`,
-    Subject: "New subscription received",
+    Message: `ID: ${body.id}, Name: ${body.fullname} got placed in ${body.company}`,
+    Subject: "New job position received",
     TopicArn: "arn:aws:sns:us-east-1:699804860351:Alumni_Got_Employed", //topic arn goes here
   };
   sns.publish(paramsSNS, context.done);
