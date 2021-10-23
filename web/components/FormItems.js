6073df41c182f4dcdad8b5e47a053bbe90cf11f9
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2021-10-24 01:32:31 +0200

    use react forms hooks for validation

--- web/components/FormItems.js
+++ web/components/FormItems.js
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import FileBase from "react-file-base64";
+import { useForm } from "react-hook-form";
+
 const initialState = {
+  id: "3",
   fullName: "",
   classNr: "",
   position: "",
@@ -8,28 +11,23 @@ const initialState = {
   aboutJob: "",
   interviewRounds: "",
   assignment: "",
-  imageUrl: "",
+  //imageUrl: "",
 };
 const FormItems = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [formData, setFormData] = useState(initialState);
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const data = { formData };
+  const submitForm = () => {
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
+      body: JSON.stringify(formData),
     };
-    fetch(
-      "https://o3fp0fun12.execute-api.us-east-1.amazonaws.com/Prod/",
-      requestOptions
-    )
+    fetch("https://o3fp0fun12.execute-api.us-east-1.amazonaws.com/Prod/", requestOptions)
       .then((res) => res.json())
       .then((res) => console.log(res));
   };
@@ -44,7 +42,13 @@ const FormItems = () => {
             alt="form/img"
             width="512"
           />
-          <form className="space-y-4 w-full md:w-1/3 rounded-lg items-center">
+          <form
+            className="space-y-4 w-full md:w-1/3 rounded-lg items-center"
+            onSubmit={handleSubmit((data) => {
+              setFormData(data);
+              submitForm();
+            })}
+          >
             <h1 className="font-bold mt-2 text-base md:text-lg text-blue-800">
               Fill out the form:
             </h1>
@@ -59,12 +63,12 @@ const FormItems = () => {
                 </label>
                 <input
                   type="text"
-                  name="fullName"
+                  {...register("fullName", { required: "Required field" })}
                   id="fullName"
-                  onChange={handleChange}
                   autoComplete="fullName"
                   className="placeholder-gray-500 pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                 />
+                {errors.fullName && <p>{errors.fullName.message}</p>}
               </div>
 
               <div className="col-span-12">
@@ -76,12 +80,12 @@ const FormItems = () => {
                 </label>
                 <input
                   type="number"
-                  name="classNr"
+                  {...register("classNr", { required: "Required field" })}
                   id="classNr"
-                  onChange={handleChange}
                   autoComplete="classNr"
                   className="placeholder-gray-500 pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                 />
+                {errors.classNr && <p>{errors.classNr.message}</p>}
               </div>
               <div className="col-span-12">
                 <label
@@ -92,28 +96,28 @@ const FormItems = () => {
                 </label>
                 <input
                   type="text"
-                  name="position"
+                  {...register("position", { required: "Required field" })}
                   id="position"
-                  onChange={handleChange}
                   autoComplete="position"
                   className="placeholder-gray-500 pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                 />
+                {errors.position && <p>{errors.position.message}</p>}
               </div>
               <div className="col-span-12">
                 <label
                   htmlFor="company"
                   className="block text-sm ml-2 font-medium text-gray-700"
                 >
-                  Company
+                  Company*
                 </label>
                 <input
                   type="text"
-                  name="company"
+                  {...register("company", { required: "Required field" })}
                   id="company"
-                  onChange={handleChange}
                   autoComplete="company"
                   className="placeholder-gray-500 pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                 />
+                {errors.company && <p>{errors.company.message}</p>}
               </div>
               <div className="col-span-12">
                 <label
@@ -125,12 +129,12 @@ const FormItems = () => {
                 </label>
                 <input
                   type="text"
-                  name="aboutJob"
+                  {...register("aboutJob", { required: "Required field" })}
                   id="aboutJob"
-                  onChange={handleChange}
                   autoComplete="aboutJob"
                   className="placeholder-gray-500 pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                 />
+                {errors.aboutJob && <p>{errors.aboutJob.message}</p>}
               </div>
               <div className="col-span-12">
                 <label
@@ -141,12 +145,16 @@ const FormItems = () => {
                 </label>
                 <input
                   type="number"
-                  name="interviewRounds"
+                  {...register("interviewRounds", {
+                    required: "Required field",
+                  })}
                   id="interviewRounds"
-                  onChange={handleChange}
-                  autoComplete="classNr"
+                  autoComplete="interviewRounds"
                   className="placeholder-gray-500 pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
                 />
+                {errors.interviewRounds && (
+                  <p>{errors.interviewRounds.message}</p>
+                )}
               </div>
             </div>
 
@@ -161,8 +169,8 @@ const FormItems = () => {
                 <input
                   type="radio"
                   className="form-radio"
-                  name="radio"
-                  value="1"
+                  {...register("assignment", { required: "Required field" })}
+                  value="yes"
                 />
                 <span className="ml-2"> Yes</span>
               </label>
@@ -170,11 +178,12 @@ const FormItems = () => {
                 <input
                   type="radio"
                   className="form-radio"
-                  name="radio"
-                  value="1"
+                  {...register("assignment", { required: "Required field" })}
+                  value="no"
                 />
                 <span className="ml-2"> No</span>
               </label>
+              {errors.assignment && <p>{errors.assignment.message}</p>}
             </div>
             <div className="flex justify-center mt-8">
               <div className="rounded-lg shadow-xl bg-gray-50 lg:w-1/1">
