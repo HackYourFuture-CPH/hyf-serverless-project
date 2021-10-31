8aa570c1cd5c49b075226eaa7edd1a07f39b9661
Patrikk Dyrberg Sørensen <patrikkdyrbergsorensen@Patrikks-MacBook-Pro.local>
2021-10-31 12:58:01 +0100

    Create initial solution of image uploading

--- web/hooks/usePicture.js
+++ web/hooks/usePicture.js
@@ -1,19 +1,34 @@
-async function uploadImage(formData) {
-  try {
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        accept: "*/*",
-        "Content-Type": "multipart/form-data",
-      },
-      body: formData,
-    };
+import axios from 'axios'
 
-    const results = await fetch("https://bdze0u9nv3.execute-api.us-east-1.amazonaws.com/default/getPresignedURL", requestOptions);
-    return results.url;
-  } catch (exception) {
-    console.log("Error", exception);
+//TODO: Switch to correct API
+const baseUrl = "https://45ehg1xwbi.execute-api.us-east-1.amazonaws.com"
+
+const getBinaryDataFromFile = async (file) => {
+  let binary = atob(file.split(',')[1])
+  let array = []
+  for (var i = 0; i < binary.length; i++) {
+    array.push(binary.charCodeAt(i))
   }
+
+  let blobData = new Blob([new Uint8Array(array)], { type: 'image/*' })
+
+  return blobData
+}
+
+
+async function uploadImage(file) {
+  // 1: get the presigned url from the api 
+  const data = await axios.get(`${baseUrl}/uploads`).data
+  const { Key, uploadUrl } = data
+
+  // 2: upload data to the url through a PUT call  
+  const blobData = getBinaryDataFromFile(file)
+
+  await axios.put(uploadUrl, blobData, {
+    headers: { "Content-Type": "image/*" }
+  })
+
+  return `${uploadUrl}/${Key}`
 }
 
 export default uploadImage;
